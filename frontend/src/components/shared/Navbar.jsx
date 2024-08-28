1d00e45db780c74d6b436cf52b443b278f845dfd
Rajeshwari Baniya <baniyarajeshwari@gmail.com>
2024-08-28 16:52:09 +0545

    UP

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -48,8 +48,8 @@ function Navbar() {
           {
             !user ? (
               <div className="flex items-center gap-2">
-                <Button variant="outline">LOGIN</Button>
-                <Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">Signup</Button>
+                <Button variant="outline">Login</Button>
+                <Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">SignUP</Button>
               </div>
             ):(
               <Popover>
