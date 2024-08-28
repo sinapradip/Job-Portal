182571291d77cb660e92c1541198df28166067ce
Rajeshwari Baniya <70877568+Rajeshwari1-1@users.noreply.github.com>
2024-08-28 16:35:20 +0545

    Update Navbar.jsx

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -48,7 +48,7 @@ function Navbar() {
           {
             !user ? (
               <div className="flex items-center gap-2">
-                <Button variant="outline">LOGIN</Button>
+                <Button variant="outline">Login</Button>
                 <Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">Signup</Button>
               </div>
             ):(
