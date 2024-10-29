4b69fddb6d81c685868a44a7dc3b72ff7c9c58a5
Rajeshwari1-1 <baniyarajeshwari@gmail>
2024-10-29 12:21:19 +0545

    front-end code added

---
+++ frontendpart/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+
+      <p className=' text-[48px] font-[700] text-[#6A38C2]'>Hello, Welcome to our Job Portal!</p>
+
+      
+     
+    </>
+  )
+}
+
+export default App
