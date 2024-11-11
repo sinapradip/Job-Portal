ce1db99d54b808a3e463061b2b2aa1a2ff5511ad
Rajeshwari Baniya <baniyarajeshwari@gmail.com>
2024-11-11 23:47:21 +0545

    fixed errors

--- frontend/README.md
+++ frontend/README.md
@@ -1,9 +1,3 @@
-# Job-Portal
-
-Welcome to our project!
-
-This is frontend code
-
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Navbar } from "./components/shared"
+
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
-
-      <p className=' text-[48px] font-[700] text-[#6A38C2]'>Hello, Welcome to our Job Portal!</p>
-
-      
-     
+      <Navbar/>
     </>
   )
 }
