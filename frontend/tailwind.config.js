e2c8d6ee1ffa1aa0a48c09f9f944bf2b7e4b697f
Rajeshwari Baniya <baniyarajeshwari@gmail.com>
2024-12-08 08:42:27 +0545

    new changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+// /** @type {import('tailwindcss').Config} */
+export default  {
   darkMode: ["class"],
   content: [
     './pages/**/*.{js,jsx}',
