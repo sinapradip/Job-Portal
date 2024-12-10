0c8724a65b49c8579be834a1102b88fbd1bac858
sina <sinapradip@gmail.com>
2024-12-10 09:21:26 +0545

    Never hardcode credentials like this

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose'
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://sinapradip:SxtPZPB0AkpQQhRc@cluster0.4s757.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        await mongoose.connect(process.env.MONGO_URI)
         console.log('MongoDB connected successfully')
     }
     catch(error) {
